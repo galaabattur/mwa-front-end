b0ee3a0b7a46e2bbdd2302011dcf2a93a3d8f873
galbadrakh_b <b.galbadrah12@gmail.com>
2020-04-06 17:42:28 -0500

    can post post.

--- src/app/news/news.component.ts
+++ src/app/news/news.component.ts
@@ -1,12 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { PostService } from '../service/post/post.service';
 
 @Component({
   selector: 'news',
   templateUrl: './news.component.html',
-  styleUrls: ['./news.component.css']
+  styleUrls: ['./news.component.css'],
 })
 export class NewsComponent implements OnInit {
-  constructor() {}
+  form: FormGroup;
+
+  constructor(private postService: PostService) {
+    this.form = new FormGroup({
+      postname: new FormControl(''),
+    });
+  }
+
+  get postname() {
+    return this.form.get('postname');
+  }
 
   ngOnInit(): void {}
+
+  newPost(data) {
+    this.postService.submitPost(data);
+  }
 }
