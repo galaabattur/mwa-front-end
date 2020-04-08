b1a621b8203d0875a5c9e7133c731bede7fe68cd
galbadrakh_b <b.galbadrah12@gmail.com>
2020-04-07 20:17:30 -0500

    showing posts, submitting posts

--- src/app/service/post/post.service.ts
+++ src/app/service/post/post.service.ts
@@ -1,25 +1,6 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
-export class PostService {
-  private postUrl = 'http://localhost:3000/api/post';
-
-  constructor(private http: HttpClient) {}
-
-  submitPost(data) {
-    const header = new HttpHeaders({ token: localStorage.getItem('token') });
-
-    this.http
-      .post(this.postUrl, data, { headers: header })
-      .subscribe((response) => {
-        console.log(response);
-      });
-  }
-
-  getPosts() {
-    console.log('getting posts');
-  }
-}
+export class PostService {}
