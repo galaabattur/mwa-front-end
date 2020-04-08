25027e4b0f7b52ab4bddade2a00150f5f7a48656
galbadrakh_b <b.galbadrah12@gmail.com>
2020-04-08 18:02:58 -0500

    user follow added

--- src/app/following/following.component.ts
+++ src/app/following/following.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
 import { UserService } from '../service/user/user.service';
 import { HttpHeaders } from '@angular/common/http';
+import * as jwt_decode from 'jwt-decode';
 
 @Component({
   selector: 'following',
@@ -12,9 +13,14 @@ export class FollowingComponent implements OnInit {
   searchForm: FormGroup;
   private header: HttpHeaders;
   searchResult: any[];
+  noresult: boolean = false;
+  username: string;
+  followed = false;
+  followText = 'Follow';
 
   constructor(private userService: UserService) {
     this.header = new HttpHeaders({ token: localStorage.getItem('token') });
+    this.username = jwt_decode(localStorage.getItem('token')).username;
     this.searchForm = new FormGroup({
       searchname: new FormControl(),
     });
@@ -28,7 +34,27 @@ export class FollowingComponent implements OnInit {
     this.userService.searchUser(formData, this.header).subscribe(
       (data) => {
         console.log(data);
-        this.searchResult = data['users'];
+        if (data['users'].length == 0) {
+          this.noresult = !this.noresult;
+        } else {
+          this.searchResult = data['users'];
+          this.noresult = false;
+        }
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+  follow(data) {
+    const sendData = { follower: data.innerHTML, username: this.username };
+    this.userService.addFollower(sendData, this.header).subscribe(
+      (data) => {
+        console.log(data);
+        if (data['msg'] === 'success') {
+          this.followed = true;
+          this.followText = 'Unfollow';
+        }
       },
       (error) => {
         console.log(error);
