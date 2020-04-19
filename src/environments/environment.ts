18b406fc22c79690f8ae8277041f2f239edbf1c7
galbadrakh_b <b.galbadrah12@gmail.com>
2020-04-18 20:20:44 -0500

    prodbvase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: false,
-  baseUrl: 'http://localhost:3000/api',
+  // baseUrl: 'http://localhost:3000/api',
+  baseUrl: 'https://mwa-project-2020-b.herokuapp.com/api',
 };
 
 /*
